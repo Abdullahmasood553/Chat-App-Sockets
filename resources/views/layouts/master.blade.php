fc2bbb4c728d72deb246428a63102e608a2d2504
abdullahmasood553 <abdullahmasood553@gmail.com>
2020-10-01 10:00:50 -0700

    chat app

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Chat App</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" >
+    <style>
+        .user-wrapper, .message-wrapper {
+            border: 1px solid #dddddd;
+            overflow-y: auto;
+        }
+
+        .user-wrapper {
+            height: 600px;
+        }
+    </style>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
